428431025217d5b833a37dc40f4909b13dc7ac1c
brightolysis <168431565+brightolysis@users.noreply.github.com>
2024-09-15 10:21:10 +0100

    updated application.properties file

--- src/main/java/com/imprint/kubernetesdemo/KubeController.java
+++ src/main/java/com/imprint/kubernetesdemo/KubeController.java
@@ -1,5 +1,6 @@
 package com.imprint.kubernetesdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1")
 public class KubeController {
+    @Value("${CLOUD_PASSWORD}")
+    private String password;
     @GetMapping("/test")
     public ResponseEntity<String> greet(){
-        return new ResponseEntity<>("hello my first kubernetes project", HttpStatus.OK);
+        String message = String.format("my test password %s",this.password);
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
